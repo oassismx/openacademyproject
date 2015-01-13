63db0496de4dcd368eaf69443aae8794b141e3dc
oassismx <omarcervantes@oassis.mx>
2015-01-12 18:12:37 -0600

    nuevo archivo python-test.py

---
+++ python-test.py
@@ -0,0 +1,9 @@
+x = int(raw_input("Por favor ingrese un número entero: "))
+if x < 0:
+    print "Numero Negativo"
+elif x == 0:
+    print "Numero Cero"
+elif x == 1:
+    print "Simple"
+else:
+    print "No fue ni negativo, ni cero, ni uno"
